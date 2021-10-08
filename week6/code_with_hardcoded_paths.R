8d06cb94537bd8b4c93656c4d15516091039998e
Andras Berczi <andras.berczi@emarsys.com>
2021-10-08 11:01:10 +0200

    Create slides for week 6 and code for correct usage of paths

---
+++ week6/code_with_hardcoded_paths.R
@@ -0,0 +1,31 @@
+library(data.table)
+
+# What is wrong with this code?
+dt <- fread("~/project/eltecon-ds/week6/data/laptop_price.csv")
+
+dt[, .N]
+
+# FYI `~` is shorthand for home directory on Mac (and Linux systems also).
+# So in my case it is the same as `/Users/i525640`
+
+# What is wrong with this code?
+setwd("~/project/eltecon-ds/week6")
+
+dt <- fread("data/laptop_price.csv")
+
+dt[, .N]
+
+# What is the proper way? Create your project with R project!
+# We use this one at Emarsys (without `here` package).
+
+
+# Optionally, use the `here package!
+
+# If you have an R project set in your project,
+# here will set the working directory to where the .Rproj file is!
+
+library(here)
+
+dt <- fread(here("week6", "data", "laptop_price.csv"))
+
+dt[, .N]
