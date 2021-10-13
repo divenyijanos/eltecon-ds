f9cc20e35c2801ee91682d7d6519aefe7a2da9b7
Andras Berczi <andras.berczi@emarsys.com>
2021-10-13 20:56:17 +0200

    Add R code for sample project

---
+++ week6/sample_project.R
@@ -0,0 +1,73 @@
+library(data.table)
+library(ggplot2)
+library(stringr)
+library(glmnet)
+
+dt <- fread("week6/data/laptop_price.csv")
+
+# 0. Setup the project!
+# 1. Is the data in the right format? Does it need cleaning?
+# 2. Visualize the distributions! Does it need more cleaning?
+# 3. Decide on features! Create train and test set!
+# 4. Use linear regression to train a model! Evaluate the model!
+# 5. Train a model using Ridge or Lasso regression (cross-validation)! Evaluate the models!
+
+
+# 1. Is the data in the right format? Does it need cleaning?
+laptop[, .N, Ram]
+laptop[, Ram_numeric := as.integer(gsub("GB", "", Ram))]
+
+laptop[, .N, word(Cpu, 1)]
+laptop[, Cpu_type := word(Cpu, 1)]
+
+laptop[, .N, word(Cpu, -1)]
+laptop[, Cpu_frequency_str := word(Cpu, -1)]
+laptop[, Cpu_frequency := as.numeric(gsub("GHz", "", word(Cpu, -1)))]
+
+laptop[, .N, word(ScreenResolution, -1)]
+laptop[, screen_resolution := word(ScreenResolution, -1)]
+
+# 2. Visualize the distributions! Does it need more cleaning
+ggplot(laptop, aes(x = Price_euros)) +
+        geom_density()
+
+ggplot(laptop, aes(x = log(Price_euros))) +
+    geom_density()
+
+laptop %>%
+    .[, .(mean_price = mean(Price_euros)), Cpu_type] %>%
+    ggplot(aes(x = Cpu_type, y = mean_price)) + geom_col()
+
+
+ggplot(laptop, aes(x = Cpu_type, y = Price_euros)) +
+    geom_boxplot()
+
+ggplot(laptop, aes(x = Ram_numeric, y = Price_euros)) +
+    geom_point() +
+    ggtitle("Compare price with Ram") +
+    labs(x = "RAM in GB", y = "Price in EUR") +
+    scale_y_continuous(labels = scales::comma)
+
+# 3. Decide on features! Create train and test set!
+set.seed(8797234)
+train_index <- sample(1:nrow(laptop), nrow(laptop) * 0.7)
+laptop_train <- laptop[train_index]
+laptop_test <- laptop[-train_index]
+
+# 4. Use linear regression to train a model! Evaluate the model!
+linear_model <- lm(
+    Price_euros ~ Cpu_type + Cpu_frequency + Ram_numeric,
+    laptop_train
+)
+summary(linear_model)
+
+laptop_test[, prediction_lm := predict(linear_model, laptop_test)]
+
+laptop_test[, sqrt(sum((Price_euros - prediction_lm)**2) / .N)]
+
+ggplot(dt_test, aes(x = Price_euros, y = prediction)) + geom_line()
+ggplot(dt_test, aes(x = Price_euros, y = prediction)) +
+    geom_point() +
+    geom_abline(slope = 1) +
+    coord_fixed(xlim = c(0, 5000), ylim = c(0, 5000))
+ggplot(dt_test, aes(x = Price_euros, y = Price_euros - prediction)) + geom_line()
